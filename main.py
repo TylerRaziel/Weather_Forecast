07899b12c3070f994a0cd0c80a6b135fe7c180c0
Ali Osman Bekiroğlu <61561002+TylerRaziel@users.noreply.github.com>
2024-04-29 02:57:28 +0300

    Loop edited

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ api_key = os.getenv('API_KEY')
 
 df = pd.read_excel('turkey.xlsx', usecols=['lat', 'lng'])
 
-json_data = []
-i = 0
+try:
+    with open('weather_data.json', 'r') as f:
+        json_data = json.load(f)
+except FileNotFoundError:
+    json_data = []
+
+flag = True
+counter = len(json_data)
+
 for index, row in df.iterrows():
     lat = row['lat']
     lng = row['lng']
@@ -24,10 +31,15 @@ for index, row in df.iterrows():
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
-        i = i + 1
-        print(i)
         data = response.json()
+        if (json_data and flag and 'last_updated' in data['current'] and
+                json_data[-1]['current']['last_updated'] == data['current']['last_updated']):
+            print('Last update time is the same as the previous data. Stopping loop.')
+            break
+        counter += 1
+        print(counter)
         json_data.append(data)
+        flag = False
     else:
         print(f'API request failed for {lat}, {lng}.')
 
@@ -35,6 +47,3 @@ with open('weather_data.json', 'w') as f:
     json.dump(json_data, f)
 
 print('Weather data saved to weather_data.json.')
-
-
-
